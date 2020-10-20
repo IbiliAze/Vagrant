b2fab37d8cdeb90af3465d2f12de696c3d968b08
Ibi Hasanli <ibili73@gmail.com>
2020-10-20 06:05:11 -0700

    ubuntu

--- Vagrant.sh
+++ Vagrant.sh
@@ -10,3 +10,23 @@ sudo vagrant halt #stop
 sudo vagrant destroy
 
 sudo vagrant destroy -f #force
+
+sudo vagrant reload
+
+sudo vagrant ssh MyBox
+
+sudo vagrant ssh-config
+
+sudo vagrant validate
+
+sudo vagrant box add ADDRESS #add a vagrant locally from the internet
+
+sudo vagrant box list
+
+sudo vagrant box outdated #show outdated boxes
+
+sudo vagrant box prune 
+
+sudo vagrant box remove MyBox
+
+
